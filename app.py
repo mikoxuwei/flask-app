ae51a4a4bb6b1e71e94859a3d5b54b7f2482d99f
mikoxuwei <s2525123a@gmail.com>
2025-06-13 16:50:26 +0800

    新增html測試

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template  # 引入render_template函式，用於渲染HTML模板
 app = Flask(__name__) #__name__代表目前執行的模組
 
 @app.route('/') # 函式的裝飾 (decorator):以函式為基礎，提供附加功能
 def home():
-    return 'Hello, Flask!'
+    return render_template("index.html")
+    # return 'Hello, Flask!'
 
 @app.route('/test') # 路由設定，當訪問/test時，會執行test函式
 def test():
